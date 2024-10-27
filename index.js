047a782d499c443bd954629d2116d6eade52f6c5
Neha Panwar <nehaxor1234@gmail.com>
2024-10-27 11:01:24 +0000

    Creating a basic server using express js.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Neha Panwar");
+// Express js:- Web framework for Node js.
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', ((req, res) => {
+    res.send("Hello World");
+}))
+
+app.get('/twitter', ((req, res) => {
+    res.send("<h1>Neha Singh</h1>");
+}))
+
+app.listen(process.env.PORT, () => {
+    console.log(`starting development server on port: ${process.env.PORT}`)
+});
