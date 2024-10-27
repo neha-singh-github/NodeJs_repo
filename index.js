b876b63eea993d12b3db07cfa522587b30e3cca8
Neha Panwar <nehaxor1234@gmail.com>
2024-10-27 11:17:34 +0000

    Added default port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', ((req, res) => {
     res.send("Hello World");
@@ -11,6 +12,6 @@ app.get('/twitter', ((req, res) => {
     res.send("<h1>Neha Singh</h1>");
 }))
 
-app.listen(process.env.PORT, () => {
-    console.log(`starting development server on port: ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`starting development server on port: ${PORT}`)
 });
